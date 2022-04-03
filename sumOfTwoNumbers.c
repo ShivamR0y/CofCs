85169e693d0ad25f2112970c41bfa47dc58cb08b
Shivam Roy <88700339+ShivamR0y@users.noreply.github.com>
2022-04-04 00:07:29 +0530

    Create sumOfTwoNumbers.c

---
+++ sumOfTwoNumbers.c
@@ -0,0 +1,13 @@
+//program for sum of two numbers
+
+#include <stdio.h>
+int main()
+{
+        float num1,num2;
+        printf("Enter two numbers to find their sum: ");
+        scanf("%f",&num1);
+        scanf("%f",&num2);
+        printf("The sum of the two nubmers is: %f",num1+num2);
+        
+        return 0;
+}
